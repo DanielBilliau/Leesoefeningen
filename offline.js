bc5fef1a72f583d622d7c04df467387f8d3b5397
DanielBilliau <daniel.billiau@telenet.be>
2026-07-29 20:06:41 +0200

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1785338888,
+	"version": 1785348203,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -65,6 +65,7 @@
 		"images/menu52-sheet0.png",
 		"images/menu53-sheet0.png",
 		"images/menu54-sheet0.png",
+		"images/menu55-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
